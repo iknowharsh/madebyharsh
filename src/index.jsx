4fe1fcfafd839cab363c5b7a73f4c438b7b12b83
Harsh <harshpatel_8@yahoo.com>
2025-04-15 22:46:54 +0000

    Add Tailwind config and styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import PortfolioPage from "./PortfolioPage.jsx";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
